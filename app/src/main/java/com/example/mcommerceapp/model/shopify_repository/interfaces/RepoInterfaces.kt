637e37a21265e92d718fbae1f4600153632793a5
Nayrakhaled <sci.nayerakhaled2018@alexu.edu.eg>
2022-05-29 14:07:06 +0200

    Actionbar update Screen

--- app/src/main/java/com/example/mcommerceapp/model/shopify_repository/interfaces/RepoInterfaces.kt
+++ app/src/main/java/com/example/mcommerceapp/model/shopify_repository/interfaces/RepoInterfaces.kt
@@ -3,8 +3,6 @@ package com.example.mcommerceapp.model.shopify_repository
 import com.example.mcommerceapp.pojo.customcollections.CustomCollections
 import com.example.mcommerceapp.pojo.products.Products
 
-
-
 interface IProducts {
     suspend fun getProducts() :ArrayList<Products>
 }
