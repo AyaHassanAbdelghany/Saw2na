900968e79fbe9f5f55751cd98a588a0290b169bc
Aya Hassan <54456549+AyaHassanAbdelghany@users.noreply.github.com>
2022-06-06 15:06:18 +0200

    add view more in home

--- app/src/main/java/com/example/mcommerceapp/MainActivity.kt
+++ app/src/main/java/com/example/mcommerceapp/MainActivity.kt
@@ -2,17 +2,24 @@ package com.example.mcommerceapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mcommerceapp.databinding.ActivityMainBinding
+import com.example.mcommerceapp.view.FragmentContainer
 import com.example.mcommerceapp.view.ui.feature_product.CategorizedProductActivity
+import com.example.mcommerceapp.view.ui.home.HomeFragment
+import com.example.mcommerceapp.view.ui.more.view.MoreFragment
+import com.example.mcommerceapp.view.ui.profile.view.Profile
 import com.example.mcommerceapp.view.ui.search.SearchActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -21,8 +28,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        Log.e("create","Hello")
         return super.onCreateOptionsMenu(menu)
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +48,6 @@ class MainActivity : AppCompatActivity() {
 
         searchImage.setOnClickListener { startActivity(Intent(this, SearchActivity::class.java)) }
 
-        val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         val appBarConfiguration = AppBarConfiguration(
@@ -49,9 +56,28 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
+        binding.navView.setOnItemSelectedListener () {
+            Log.e("selected","hello")
+            when(it.itemId){
+                R.id.navigation_home -> {setCurrentFragment(Profile())
+                    setCurrentFragment(FragmentContainer())
+                }
+                R.id.navigation_profile -> setCurrentFragment(Profile())
+                R.id.navigation_setting -> setCurrentFragment(MoreFragment())
+
+            }
+            true
+        }
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+
+
     }
 
-
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+           replace(R.id.nav_host_fragment_activity_main,fragment)
+            commit()
+        }
 }
+
--- app/src/main/java/com/example/mcommerceapp/view/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mcommerceapp/view/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mcommerceapp.view.ui.home
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -10,6 +11,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
@@ -20,6 +22,7 @@ import com.example.mcommerceapp.databinding.FragmentHomeBinding
 import com.example.mcommerceapp.model.Keys
 import com.example.mcommerceapp.model.remote_source.RemoteSource
 import com.example.mcommerceapp.model.shopify_repository.product.ProductRepo
+import com.example.mcommerceapp.view.ui.feature_product.CategorizedProductActivity
 import com.example.mcommerceapp.view.ui.home.adapter.AdvAdapter
 import com.example.mcommerceapp.view.ui.home.adapter.CollectionAdpater
 import com.example.mcommerceapp.view.ui.home.adapter.OnClickListner
@@ -49,12 +52,22 @@ class HomeFragment() : OnClickListner,Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         init()
 
+         binding.catogeryViewMoreTextView.setOnClickListener(){
+
+             val bundle = Bundle()
+             bundle.putString("VALUE","")
+             bundle.putString("TYPE", Keys.COLLECTION)
+            findNavController(requireView())?.navigate(R.id.actCategoryMore,bundle);
+         }
+
         return binding.root
     }
 
 
     override fun onResume() {
         super.onResume()
+        Log.e("home","hello")
+
         homeVM.getProduct(Keys.PRODUCT_TYPE)
         observerVendors()
         observerCollections()
@@ -127,13 +140,22 @@ class HomeFragment() : OnClickListner,Fragment() {
 
     override fun onClick(value: String?,type:String) {
         val bundle = Bundle()
-        bundle.putString("VALUE", value)
-        bundle.putString("TYPE", type)
-        findNavController(this)?.navigate(R.id.actCategory,bundle);
+
+        when(type){
+            Keys.VENDOR ->  {
+                bundle.putString("VALUE", value)
+                bundle.putString("TYPE", type)
+                findNavController(requireView())?.navigate(R.id.actCategory,bundle)
+            }
+            Keys.COLLECTION ->  {
+                bundle.putString("PRODUCT_TYPE", value)
+                bundle.putString("TYPE", type)
+                val intent = Intent(requireContext(), CategorizedProductActivity::class.java)
+                intent.putExtra("PRODUCTS", bundle)
+                startActivity(intent)
+            }
+        }
 
     }
-    private fun findNavController(fragment: Fragment): NavController? {
-        val view = fragment.view
-        return findNavController(view!!)
-    }
+
 }
