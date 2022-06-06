e4b495a448707863b1d206923a7fc411c2727232
Nayrakhaled <sci.nayerakhaled2018@alexu.edu.eg>
2022-06-06 18:24:19 +0200

    integrate favourite to product detail

--- app/src/main/java/com/example/mcommerceapp/network/ShopifyService.kt
+++ app/src/main/java/com/example/mcommerceapp/network/ShopifyService.kt
@@ -92,11 +92,6 @@ interface ShopifyService {
         @Query("vendor") vendor: String
     ): Response<JsonObject>
 
-        @Query("fields") fields: String
-        ,@Query("collection_id") collection_id :String
-       ,@Query("vendor") vendor :String
-    ):  Response<JsonObject>
-
     @Headers(
         "X-Shopify-Access-Token: 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",
         "Content-Type: application/json"
