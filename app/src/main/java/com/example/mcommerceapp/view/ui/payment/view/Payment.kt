56ee71888790c92b593bfd88db56eaccfab3ab4f
Marwan <Marwaanadelabdelrehim@gmail.com>
2022-06-07 02:32:00 +0200

    Payment

---
+++ app/src/main/java/com/example/mcommerceapp/view/ui/payment/view/Payment.kt
@@ -0,0 +1,172 @@
+package com.example.mcommerceapp.view.ui.payment.view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.mcommerceapp.R
+import com.example.mcommerceapp.databinding.ActivityPaymentBinding
+import com.example.mcommerceapp.view.ui.payment.viewmodel.CheckoutViewModel
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.wallet.PaymentData
+import org.json.JSONException
+import org.json.JSONObject
+
+class Payment : AppCompatActivity() {
+
+    private val model: CheckoutViewModel by viewModels()
+
+    private lateinit var layout: ActivityPaymentBinding
+    private lateinit var googlePayButton: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Use view binding to access the UI elements
+        layout = ActivityPaymentBinding.inflate(layoutInflater)
+        setContentView(layout.root)
+
+        // Setup buttons
+        googlePayButton = layout.googlePayButton.root
+        googlePayButton.setOnClickListener { requestPayment() }
+
+        // Check Google Pay availability
+        model.canUseGooglePay.observe(this, Observer(::setGooglePayAvailable))
+    }
+
+    /**
+     * If isReadyToPay returned `true`, show the button and hide the "checking" text. Otherwise,
+     * notify the user that Google Pay is not available. Please adjust to fit in with your current
+     * user flow. You are not required to explicitly let the user know if isReadyToPay returns `false`.
+     *
+     * @param available isReadyToPay API response.
+     */
+    private fun setGooglePayAvailable(available: Boolean) {
+        if (available) {
+            googlePayButton.visibility = View.VISIBLE
+        } else {
+            Toast.makeText(
+                this,
+                R.string.google_pay_status_unavailable,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun requestPayment() {
+
+        // Disables the button to prevent multiple clicks.
+        googlePayButton.isClickable = false
+
+        // The price provided to the API should include taxes and shipping.
+        // This price is not displayed to the user.
+        val dummyPriceCents = 100L
+        val shippingCostCents = 900L
+        val task = model.getLoadPaymentDataTask(dummyPriceCents + shippingCostCents)
+
+        task.addOnCompleteListener { completedTask ->
+            if (completedTask.isSuccessful) {
+                completedTask.result.let(::handlePaymentSuccess)
+            } else {
+                layout.statusImg.setImageResource(R.drawable.unpaid)
+                when (val exception = completedTask.exception) {
+                    is ResolvableApiException -> {
+                        resolvePaymentForResult.launch(
+                            IntentSenderRequest.Builder(exception.resolution).build()
+                        )
+                    }
+                    is ApiException -> {
+                        handleError(exception.statusCode, exception.message)
+                    }
+                    else -> {
+                        handleError(
+                            CommonStatusCodes.INTERNAL_ERROR, "Unexpected non API" +
+                                    " exception when trying to deliver the task result to an activity!"
+                        )
+                    }
+                }
+            }
+
+            // Re-enables the Google Pay payment button.
+            googlePayButton.isClickable = true
+        }
+    }
+
+    // Handle potential conflict from calling loadPaymentData
+    private val resolvePaymentForResult =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result: ActivityResult ->
+            when (result.resultCode) {
+                RESULT_OK ->
+                    result.data?.let { intent ->
+                        PaymentData.getFromIntent(intent)?.let(::handlePaymentSuccess)
+                    }
+
+                RESULT_CANCELED -> {
+                    // The user cancelled the payment attempt
+                    layout.statusImg.setImageResource(R.drawable.unpaid)
+                }
+            }
+        }
+
+    /**
+     * PaymentData response object contains the payment information, as well as any additional
+     * requested information, such as billing and shipping address.
+     *
+     * @param paymentData A response object returned by Google after a payer approves payment.
+     * @see [Payment
+     * Data](https://developers.google.com/pay/api/android/reference/object.PaymentData)
+     */
+    private fun handlePaymentSuccess(paymentData: PaymentData) {
+        val paymentInformation = paymentData.toJson()
+
+        try {
+            // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
+            val paymentMethodData =
+                JSONObject(paymentInformation).getJSONObject("paymentMethodData")
+            val billingName = paymentMethodData.getJSONObject("info")
+                .getJSONObject("billingAddress").getString("name")
+            Log.d("BillingName", billingName)
+
+            layout.statusImg.setImageResource(R.drawable.paid)
+
+            Toast.makeText(
+                this,
+                getString(R.string.payments_show_name, billingName),
+                Toast.LENGTH_LONG
+            ).show()
+
+            // Logging token string.
+            Log.d(
+                "Google Pay token", paymentMethodData
+                    .getJSONObject("tokenizationData")
+                    .getString("token")
+            )
+
+        } catch (error: JSONException) {
+            Log.e("handlePaymentSuccess", "Error: $error")
+        }
+    }
+
+    /**
+     * At this stage, the user has already seen a popup informing them an error occurred. Normally,
+     * only logging is required.
+     *
+     * @param statusCode will hold the value of any constant from CommonStatusCode or one of the
+     * WalletConstants.ERROR_CODE_* constants.
+     * @see [
+     * Wallet Constants Library](https://developers.google.com/android/reference/com/google/android/gms/wallet/WalletConstants.constant-summary)
+     */
+    private fun handleError(statusCode: Int, message: String?) {
+        Log.e("Google Pay API error", "Error code: $statusCode, Message: $message")
+    }
+
+
+}
--- app/src/main/java/com/example/mcommerceapp/view/ui/shopping_cart/view/ShoppingCartScreen.kt
+++ app/src/main/java/com/example/mcommerceapp/view/ui/shopping_cart/view/ShoppingCartScreen.kt
@@ -1,14 +1,18 @@
 package com.example.mcommerceapp.view.ui.shopping_cart.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mcommerceapp.R
+import com.example.mcommerceapp.view.ui.payment.view.Payment
 
 class ShoppingCartScreen : AppCompatActivity() {
     private lateinit var cartItemsRecyclerView: RecyclerView
     private lateinit var cartItemsAdapter: CartItemsAdapter
+    private lateinit var checkoutBt: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,10 @@ class ShoppingCartScreen : AppCompatActivity() {
 
         cartItemsAdapter = CartItemsAdapter(this)
         cartItemsRecyclerView.adapter = cartItemsAdapter
+
+        checkoutBt = findViewById(R.id.checkout_bt)
+        checkoutBt.setOnClickListener {
+            startActivity(Intent(this, Payment::class.java))
+        }
     }
 }
